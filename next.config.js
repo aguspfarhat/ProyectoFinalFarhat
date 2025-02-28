5dd503c2ee26ed17d9cbed0e4d8126fbf4bbe071
cacho <aguspfarhat02@gmail.com>
2025-02-28 15:20:04 -0300

    Ultimas pruebas

--- next.config.js
+++ next.config.js
@@ -26,31 +26,55 @@
 // export default nextConfig;
 
 
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     eslint: {
+//         // Warning: esto permite que el build continúe aunque existan errores de ESLint.
+//         ignoreDuringBuilds: true,
+//     },
+//     images: {
+//         domains: ["res.cloudinary.com"],
+//     },
+//     webpack: (config) => {
+//         // Permitir que Webpack procese archivos .mjs
+//         config.module.rules.push({
+//             test: /\.mjs$/,
+//             include: /node_modules/,
+//             type: 'javascript/auto',
+//         });
+
+//         // Asegurarse de resolver archivos con extensión .mjs
+//         config.resolve.extensions.push('.mjs');
+
+//         return config;
+//     },
+// };
+
+// export default nextConfig;
+
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        // Warning: esto permite que el build continúe aunque existan errores de ESLint.
         ignoreDuringBuilds: true,
     },
     images: {
         domains: ["res.cloudinary.com"],
     },
     webpack: (config) => {
-        // Permitir que Webpack procese archivos .mjs
         config.module.rules.push({
             test: /\.mjs$/,
             include: /node_modules/,
             type: 'javascript/auto',
         });
 
-        // Asegurarse de resolver archivos con extensión .mjs
         config.resolve.extensions.push('.mjs');
 
         return config;
     },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
